70a7f54d1109b77a4a7800a576881d07626a551f
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2019-05-03 17:58:37 +0900

    GPS

---
+++ Cteam/Assets/SgChan/GPS.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GetLocation", 1,5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetLocation()
+    {
+        Input.location.Start();
+        var location= Input.location.lastData.longitude;
+        if (location != 0)
+        {
+
+        print(location);
+        }
+    }
+}
