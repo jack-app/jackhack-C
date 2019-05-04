1a6324914293e5a8efb4407d0980a04889d6d9f5
LAPTOP-MAIOGCSD\kazuki <nekonekonya.kuro@gmail.com>
2019-05-04 13:58:37 +0900

    修正

---
+++ Cteam/Assets/Fuji/BombTiedScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BombTiedScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DateTime Getup = Data.getuptime;
+        DateTime Departure = Data.getouttime;
+        DateTime Now = DateTime.Now;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
