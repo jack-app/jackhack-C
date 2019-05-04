480b871c25bbaff3c938a03f3b4330cf639c60bc
LAPTOP-MAIOGCSD\kazuki <nekonekonya.kuro@gmail.com>
2019-05-04 11:14:02 +0900

    時間設定の修正

---
+++ Cteam/Assets/kuroneko/TommorowScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TommorowScript : MonoBehaviour
+{
+    Button btn;
+    public static int flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = GameObject.Find("TommorowButton").GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        flag = 1;
+        button
+    }
+}
