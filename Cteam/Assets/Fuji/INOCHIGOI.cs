fe07b601e0caff8452bdd7a0199d572d92a15aaf
RockHopper2516 <yf20000fy@i.softbank.jp>
2019-05-04 21:01:53 +0900

    縛られ画面でタップすると3か所からランダムで犬の声がきけるかも

---
+++ Cteam/Assets/Fuji/INOCHIGOI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class INOCHIGOI : MonoBehaviour
+{
+   public GameObject[] TypesOfHelps;
+    public int min = 1;
+    public int max = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+        Touch T = Input.GetTouch(0);
+        if (T.phase == TouchPhase.Began) {
+            int HelpNUM = Random.Range(min, max);
+            Instantiate(TypesOfHelps[HelpNUM]);
+    } }
+}
