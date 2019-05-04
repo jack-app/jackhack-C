eb02ed7e75b6c3d5cd86a963f288848f6ca1b9e8
LAPTOP-MAIOGCSD\kazuki <nekonekonya.kuro@gmail.com>
2019-05-04 19:36:41 +0900

    名前

--- Cteam/Assets/kuroneko/WriteName.cs
+++ Cteam/Assets/kuroneko/WriteName.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class SettingFinishScript : MonoBehaviour
+public class WriteName : MonoBehaviour
 {
     // Start is called before the first frame update
+    Text nameText;
+
     void Start()
     {
-        
+        nameText = GameObject.Find("nameText").GetComponent<Text>();
+        nameText.text = Data.dogname;
     }
 
     // Update is called once per frame
@@ -18,11 +22,6 @@ public class SettingFinishScript : MonoBehaviour
     }
 
     public void OnClick()
-    {
-        SceneManager.LoadScene("MainScene");
-    }
-
-    public void NameClick()
     {
         SceneManager.LoadScene("NameScene");
     }
