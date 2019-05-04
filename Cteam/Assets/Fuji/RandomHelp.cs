5752b1f960e9360b19bf5392a67ca8b5bf91fdd9
LAPTOP-MAIOGCSD\kazuki <nekonekonya.kuro@gmail.com>
2019-05-04 21:08:55 +0900

    audio

---
+++ Cteam/Assets/Fuji/RandomHelp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHelp : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int RANNUM = Random.Range(1, 4);
+        var BornSpot = GameObject.Find("RandomSpot" + RANNUM);
+        Vector3 pos = BornSpot.transform.position;
+        this.transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
