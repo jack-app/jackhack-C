f871df03035fc6aee3c6821916dcdbe77048ad7d
RockHopper2516 <yf20000fy@i.softbank.jp>
2019-05-05 12:02:39 +0900

    のーこめんと

---
+++ Cteam/Assets/Fuji/MatsudaiFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatsudaiFade : MonoBehaviour
+{
+    public Text Matsudai;
+    public float FadeAmount = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float A = 1f;
+        if(A == 0f) { FadeAmount = FadeAmount * -1;
+        }
+        if(A== 255f) { FadeAmount = FadeAmount * -1;
+        }
+        A = A + FadeAmount * Time.deltaTime;
+        Matsudai.color = new Color(0, 0, 0, A);
+    }
+}
