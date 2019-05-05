eea19e072d1ce3d15835dcb763c5bde1a9992090
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2019-05-05 13:46:04 +0900

    BGMと成功ボタン

---
+++ Cteam/Assets/SgChan/PreSucceed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreSucceed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Succeed()
+    {
+        Data.alarm = false;
+        PlayerPrefs.SetInt("Alarm", 0);
+        Data.getupdays += 1;
+        PlayerPrefs.SetInt("GetUpDays", Data.getupdays);
+        SceneManager.LoadScene("Succeed");
+    }
+}
